27acaa204898382a4f4ed248ed0a4039b9eba964
Yassir Al-Refaie <yassir.alrefaie@gmail.com>
2015-09-10 16:06:53 +0900

    Added some PRobing signals to be able to write TB

--- src/test/top_tb.sv
+++ src/test/top_tb.sv
@@ -7,7 +7,32 @@ module top_tb;
    logic [1:0] traffic_state;
    logic [2:0] request_floor;
    logic       request_dir;
- int 	       stim_counter = 0;
+   int 	       stim_counter = 0;
+
+   logic [2:0] tb_current_floor_1;
+   logic [2:0] tb_current_floor_2;
+   logic [1:0] tb_state_elev_1;
+   logic [1:0] tb_state_elev_2;
+   logic       tb_current_dir_1;
+   logic       tb_current_dir_2;
+   
+   logic [6:0] tb_button_panel_1;
+   logic [6:0] tb_button_panel_2;
+   
+   //Signals needed to figure out when the passenger will
+   // issue a command from inside the elevator
+   assign tb_current_floor_1 = DUT.current_floor_elev_1;
+   assign tb_current_floor_2 = DUT.current_floor_elev_2;
+   assign tb_state_elev_1 = DUT.ELEVATOR_1.MODEL.state;
+   assign tb_state_elev_2 = DUT.ELEVATOR_2.MODEL.state;
+   assign tb_current_dir_1 = DUT.current_dir_elev_1;
+   assign tb_current_dir_2 - DUT.current_dir_elev_2;
+   
+   //Force the button panel to select the target destination
+   force DUT.ELEVATOR_1.PANEL.buttons = tb_button_panel_1;
+   force DUT.ELEVATOR_2.PANEL.buttons = tb_button_panel_2;
+   
+   
    
    top DUT (.*);
 
@@ -15,9 +40,9 @@ module top_tb;
      begin
 	clk=0;
 	reset=1;
-#10 reset = 0;
+	#10 reset = 0;
      end
-always #10 clk = !clk;
+   always #10 clk = !clk;
    always_ff@(posedge clk)
      begin
 	request<=0;
@@ -34,7 +59,7 @@ always #10 clk = !clk;
 	       request<=1;
 	    end
 	  66:
-	  begin
+	    begin
 	       request_floor<=5;
 	       request_dir<=0;
 	       request<=1;
